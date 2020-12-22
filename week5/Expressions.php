827e849895e390128034f9c98813d3ef511818ef
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-12-22 21:38:09 +0100

    week 5 - php example file for Expressions

---
+++ week5/Expressions.php
@@ -0,0 +1,10 @@
+<?php
+$x = "15" + 27; // 42
+echo ($x);
+echo ("\n\n\n");
+$l = 12;
+$y = 15 + $l++;
+echo "l is $l and y is $y \n";
+
+$a = 'Hello '. 'World'. '!' . 2000; // string concatenation
+echo $a. "\n";
