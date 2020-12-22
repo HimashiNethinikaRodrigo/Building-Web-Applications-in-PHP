13ed62b912aee2756d78e183dafbd899e9d61f5e
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-12-22 21:37:54 +0100

    week 5 - php example file for DoubleQuotes

---
+++ week5/DoubleQuoteExample.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "this is a simple string";
+echo "\n";
+echo "You can also have embedded newlines in 
+strings this way as ut us 
+okay to do";
+
+// Outputs: This will expand:
+//           a newline
+
+echo "This will expand: \na newline";
+
+# Outputs: Variables do 12
+$expand = 12;
+
+echo "Variables do $expand\n";
