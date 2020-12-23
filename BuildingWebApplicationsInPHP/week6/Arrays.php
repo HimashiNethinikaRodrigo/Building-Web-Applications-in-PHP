14553de9a091fcb7d015a0d91ed5728c29ec81ba
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-12-23 20:22:22 +0100

    week 6 array functions

--- BuildingWebApplicationsInPHP/week6/Arrays.php
+++ BuildingWebApplicationsInPHP/week6/Arrays.php
@@ -11,4 +11,47 @@
     echo ("<pre>\n");
 
     var_dump($stuff2);
-?>
+
+    $va = array();
+    $va[] = "Hello";
+    $va[] = "Run";
+    $va[] = "Sing";
+    $va[] = "Happy";
+    $va[] = "Swim";
+    $va[] = "Read";
+    $va[] = "Apple";
+    $va[] = "Kiwi";
+    print_r($va);
+
+    $za= array();
+    $za["name"] = "Himashi";
+    $za["course"] = "Web Application";
+    print_r($va);
+
+//    Looping through array
+    foreach ($stuff2 as  $key => $value){
+        echo "Key= ", $key, ", Value= ", $value, "\n";
+    }
+
+    for ($i = 0; $i < count($va); $i++ ){
+        echo "i= ", $i, " value= ", $va[$i], "\n";
+    }
+
+//    Array key exist
+    array_key_exists("name", $za);
+    isset($za["name"]);
+
+//    count
+    count($za);
+
+//    check if the object is an array
+    is_array($va);
+
+//    sort array values
+    sort($za); // sort array by value, losses key
+    ksort($za); // sort array by key
+    asort($za); // sort array by value
+
+//    shuffle array into random order
+    shuffle($va);
+
