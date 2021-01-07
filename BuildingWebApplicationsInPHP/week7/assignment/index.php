ad6ba0de4bf7b84159020b31ddc879d926f8b867
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2021-01-07 12:26:33 +0100

    week 7 hashing pin reversing code change source code link

--- BuildingWebApplicationsInPHP/week7/assignment/index.php
+++ BuildingWebApplicationsInPHP/week7/assignment/index.php
@@ -75,7 +75,7 @@ if ( isset($_GET['md5']) ) {
     <li><a href="index.php">Reset</a></li>
     <li><a href="md5.php">MD5 Encoder</a></li>
     <li><a href="makecode.php">MD5 Code Maker</a></li>
-    <li><a href="https://github.com/csev/wa4e/tree/master/code/crack"
+    <li><a href="https://github.com/HimashiNethinikaRodrigo/Building-Web-Applications-in-PHP-answer-key/tree/main/BuildingWebApplicationsInPHP/week7"
            target="_blank">Source code for this application</a></li>
 </ul>
 </body>
